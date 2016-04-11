a7a1b8dd536848ec904d63fba73739a6ce82f57f
elaineliu17 <elaineliu17@users.noreply.github.com>
2016-04-10 20:10:06 -0400

    Update Result.java

--- src/Result.java
+++ src/Result.java
@@ -3,6 +3,8 @@ import java.util.List;
 public class Result {
     List<ProdIntro> introTestSet;
     List<ProdSelection> selectionTestSet;
+    List<ProdIntroReal> introRealTestSet;
     List<Integer> resultSet;
+    List<Double> resultRealSet;
     double accuracy;
 }
